f152f7b3e1f2cd173325bcf716f1180bde012337
Shray Rastogi <shray45@gmail.com>
2018-02-11 00:15:17 +0530

    Adds the route config

--- AngularJS-Typescript/src/assets/scripts/app/app.ts
+++ AngularJS-Typescript/src/assets/scripts/app/app.ts
@@ -1,3 +1,24 @@
 namespace app {
-  angular.module("productManagement", []);
+  let app = angular.module("productManagement", [
+    "ngRoute",
+    "productResourceMock",
+    "common.service"
+  ]);
+
+  app.config(routeConfig);
+  routeConfig.$inject = ["$routeProvider"];
+  function routeConfig($routeProvider: ng.route.IRouteProvider): void {
+    $routeProvider
+      .when("/productList", {
+        templateUrl:
+          "/AngularJS-Typescript/src/assets/scripts/app/modules/productList/productListView.html",
+        controller: "ProductListController as vm"
+      })
+      .when("/productDetail/:productId", {
+        templateUrl:
+          "/AngularJS-Typescript/src/assets/scripts/app/modules/productDetail/productDetailView.html",
+        controller: "ProductDetailController as vm"
+      })
+      .otherwise({ redirectTo: "/productList" });
+  }
 }
