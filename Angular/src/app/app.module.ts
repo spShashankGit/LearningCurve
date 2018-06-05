b59b7a238685c2e05aa3fcbce81512d348c76988
Shray Rastogi <shray45@gmail.com>
2018-06-05 23:34:34 +0530

    Imports Pipe and FormsModule

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
