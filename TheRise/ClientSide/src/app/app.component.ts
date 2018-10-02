3bbf44f7e23d72bc699150c10c8bf8db25213597
ShrayRastogi <shray45@gmail.com>
2018-10-02 18:31:51 +0530

    Client Side and Web Api folder structure

---
+++ TheRise/ClientSide/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Http, Response } from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  greetings = '';
+
+  constructor(private _appService: AppService) {}
+
+  ngOnInit(): void {
+    this._appService.sayHello().subscribe(result => {
+      this.greetings = result;
+    });
+  }
+}
---
+++ TheRise/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greetings()
+        {
+            return Ok("Hello from ASP.NET Core Web API.");
+        }
+
+    }
+}
