af22a9105e7f24eecb0e8f8a807b311df6ec65f5
ShrayRastogi <shray45@gmail.com>
2018-10-02 20:02:53 +0530

    Name changes & deletes old code

--- AngularJS-Typescript/src/assets/scripts/app/modules/common/services/common.module.ts
+++
@@ -1,8 +0,0 @@
-import * as angular from "angular";
-import { DataAccessService } from "./dataAccess.service";
-
-let common = angular.module("common.module", ["ngResource"]);
-
-common.service("dataAccessService", DataAccessService);
-
-export default common;
