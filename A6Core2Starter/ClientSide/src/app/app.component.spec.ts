84b7b9843b1a8a46bb7f61021ca6a89938fba5b1
spShashankGit <shashank_pandey93@yahoo.com>
2019-01-02 03:07:53 +0100

    sample changes

--- A6Core2Starter/ClientSide/src/app/app.component.spec.ts
+++ A6Core2Starter/ClientSide/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -13,15 +14,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'hercules'`, async(() => {
+  it(`should have as title 'Welcome'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hercules');
+    expect(app.title).toEqual('Welcome');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hercules!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Learning Curve!');
   }));
 });
